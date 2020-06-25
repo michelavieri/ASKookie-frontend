4c23601512ad1aa84255e01afc9a0985435ddd35
Michela Vieri <michela.vieri.hp@gmail.com>
2020-06-26 00:00:30 +0800

    revert fetch home

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ export class Home extends Component {
         };
     }
     componentDidMount() {
-        fetch('https://compassionate-shockley-c03462.netlify.app/https://whispering-hamlet-08619.herokuapp.com/home')
+        fetch('https://whispering-hamlet-08619.herokuapp.com/home')
             .then(res => res.json())
             .then(res => {
                 this.setState({ feeds: res.data }, () => console.log('Data fetched', res));

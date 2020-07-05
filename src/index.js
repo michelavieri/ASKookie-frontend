f75c696acb7db2b76a0454af058a5eb63105bcdb
Michela Vieri <michela.vieri.hp@gmail.com>
2020-07-05 19:29:03 +0800

    add spinner in index js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
+
+const LoadingIndicator = props => {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    promiseInProgress &&
+    <div
+      style={{
+        width: "100%",
+        height: "100",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }
+      }
+    >
+      <Loader type="ThreeDots" color="#FC5D42" height="100" width="100" />
+    </div >
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>

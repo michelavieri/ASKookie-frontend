afa089dada7efa272e67e989242144c5600e40be
Michela Vieri <michela.vieri.hp@gmail.com>
2020-07-23 00:38:34 +0800

    delete unneccessary in nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,23 +5,6 @@ import logo from '../logo.png';
 import profilePicture from '../default_pp.png';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
-import Select from 'react-select';
-
-import Button from '@atlaskit/button';
-import { trackPromise } from 'react-promise-tracker';
-
-
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { defaultTheme } from 'react-select';
-
-
-const { colors } = defaultTheme;
-
-const selectStyles = {
-    control: provided => ({ ...provided, minWidth: 240, margin: 8 }),
-    menu: () => ({ boxShadow: 'inset 0 1px 0 rgba(0, 0, 0, 0.1)' }),
-};
 
 class Navigation extends Component {
     logOut(e) {
@@ -58,28 +41,6 @@ class Navigation extends Component {
         this.handleSubmitAsk = this.handleSubmitAsk.bind(this);
         this.handleSubmitPost = this.handleSubmitPost.bind(this);
     }
-
-    // componentDidMount() {
-    //     if (localStorage.usertoken) {
-    //         const token = localStorage.usertoken;
-    //         const decoded = jwt_decode(token);
-    //         this.setState({ name: decoded.result.username });
-    //     }
-    //     trackPromise(
-    //         fetch('http://localhost:5000/home')
-    //             .then(res => res.json())
-    //             .then(res => {
-    //                 this.setState({ feeds_nav: res.data });
-    //             }))
-    // }
-
-    toggleOpen = () => {
-        this.setState(state => ({ isOpen: !state.isOpen }));
-    };
-    onSelectChange = value => {
-        this.toggleOpen();
-        this.setState({ value });
-    };
 
     onPostChange = e => {
         const token = localStorage.usertoken;
@@ -219,58 +180,6 @@ class Navigation extends Component {
         });
         console.log("query", this.state.query)
     }
-
-    handleSearch() {
-        this.props.history.push(`/signinform`)
-    }
-
-    // handleInputChange = e => {
-    //     console.log("query", e.target.value)
-    //     this.toggleOpen();
-
-    //     this.setState({
-    //         query: e.target.value
-    //     });
-
-    //     axios.get('http://localhost:5000/search/' + `${this.state.query}`)
-    //         .then(res => {
-    //             const filtered_Data = res.data;
-    //             console.log("filteredData", res.data)
-    //             // .then(response => response.json())
-    //             // .then(feeds_nav => {
-    //             //     const { query } = this.state;
-    //             //     const filteredData = feeds_nav && feeds_nav.filter(element => {
-    //             //         return element.question.toLowerCase().includes(query.toLowerCase()) 
-    //             //         || element.title.toLowerCase().includes(query.toLowerCase());
-    //             //     });
-    //             //     console.log("filteredData", filteredData);
-
-    //             this.setState({
-    //                 filteredData: filtered_Data,
-    //             });
-    //         });
-    // }
-
-    // search = () => {
-    //     axios.get('http://localhost:5000/search' + `${this.state.query}`)
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             const filtered_Data = res.data.data;
-    //             console.log("filteredData", res.data.data)
-    //             // .then(response => response.json())
-    //             // .then(feeds_nav => {
-    //             //     const { query } = this.state;
-    //             //     const filteredData = feeds_nav && feeds_nav.filter(element => {
-    //             //         return element.question.toLowerCase().includes(query.toLowerCase()) 
-    //             //         || element.title.toLowerCase().includes(query.toLowerCase());
-    //             //     });
-    //             //     console.log("filteredData", filteredData);
-
-    //             this.setState({
-    //                 filteredData: filtered_Data,
-    //             });
-    //         });
-    // };
 
     getUsername() {
         const token = localStorage.usertoken
@@ -618,72 +527,6 @@ class Navigation extends Component {
         )
     }
 }
-
-const Menu = props => {
-    const shadow = 'hsla(218, 50%, 10%, 0.1)';
-    return (
-        <div
-            css={{
-                backgroundColor: 'white',
-                borderRadius: 4,
-                boxShadow: `0 0 0 1px ${shadow}, 0 4px 11px ${shadow}`,
-                marginTop: 8,
-                position: 'absolute',
-                zIndex: 2,
-            }}
-            {...props}
-        />
-    );
-};
-const Blanket = props => (
-    <div
-        css={{
-            bottom: 0,
-            left: 0,
-            top: 0,
-            right: 0,
-            position: 'fixed',
-            zIndex: 1,
-        }}
-        {...props}
-    />
-);
-const Dropdown = ({ children, isOpen, target, onClose }) => (
-    <div css={{ position: 'relative' }}>
-        {target}
-        {isOpen ? <Menu>{children}</Menu> : null}
-        {isOpen ? <Blanket onClick={onClose} /> : null}
-    </div>
-);
-const Svg = p => (
-    <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        focusable="false"
-        role="presentation"
-        {...p}
-    />
-);
-const DropdownIndicator = () => (
-    <div css={{ color: colors.neutral20, height: 24, width: 32 }}>
-        <Svg>
-            <path
-                d="M16.436 15.085l3.94 4.01a1 1 0 0 1-1.425 1.402l-3.938-4.006a7.5 7.5 0 1 1 1.423-1.406zM10.5 16a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11z"
-                fill="currentColor"
-                fillRule="evenodd"
-            />
-        </Svg>
-    </div>
-);
-const ChevronDown = () => (
-    <Svg style={{ marginRight: -6 }}>
-        <path
-            d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z"
-            fill="currentColor"
-            fillRule="evenodd"
-        />
-    </Svg>)
 
 const NavigationRouter = withRouter(Navigation);
 export default NavigationRouter;
